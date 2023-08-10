5d81e5576d7bbac8b252cf5f64a91dbd4e90e803
Osha Foster <31053423+OshaFoster@users.noreply.github.com>
2023-08-10 10:53:58 -0600

    basic shape of website

--- app/page.js
+++ app/page.js
@@ -1,113 +1,137 @@
-import Image from 'next/image'
+'use client';
+import Image from 'next/image';
+import { useRef } from 'react';
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  MotionValue,
+} from 'framer-motion';
+
+const titles = [
+  {
+    title: 'Medical content researcher and reviewer for The Ruscio Institute',
+    years: '03yrs',
+  },
+  { title: 'Biologist', years: '10yrs' },
+  { title: 'Editor and writer', years: '11yrs' },
+  { title: 'Journalist', years: '02yrs' },
+  { title: 'Student of medicine', years: '02yrs' },
+  { title: 'Marriage & Family Therapist trainee', years: '01yr' },
+];
+
+function useParallax(value, start, end) {
+  return useTransform(value, [0, 1], [start, end]);
+}
+
+function Title({ item }) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll(ref);
+
+  const y = useParallax(scrollYProgress, -10, 0); // Adjust the start and end values
+
+  return (
+    <section className='flex flex-row border-b border-slate-800'>
+      <div className='flex flex-row  p-5'>
+        <h1 className='text-2xl font-extralight'>{item.title}</h1>
+      </div>
+      <div className='flex flex-row pt-6' ref={ref}>
+        <motion.h2
+          className='text-2xl font-extralight text-yellow-600'
+          style={{ y }}
+        >
+          {item.years}
+        </motion.h2>
+      </div>
+    </section>
+  );
+}
+
 
 export default function Home() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex flex-col w-screen background'>
+      <div class='border border-slate-800 flex flex-row h-90vh'>
+        <div class='px-12 flex flex-col w-4/6 border-r border-slate-800'>
+          <div class='py-24'>
+            <h2 class='text-2xl font-extralight'>
+              Medical research is{' '}
+              <span class='font-normal text-yellow-600'>hard</span> to interpret
+              correctly and apply to real-life health situations. Yet,
+              practicing evidence-based health care requires knowing what the
+              evidence <span class='font-normal text-yellow-600'>means</span> so
+              you can answer specific questions about health.
+            </h2>
+          </div>
+          <div class='py-8 top-0 sticky z-40 background'>
+            <h1 class='text-6xl font-extralight font-serif text-yellow-600'>
+              Hilary Watts
+            </h1>
+            <h1 class='text-2xl font-extralight font-serif text-yellow-600'>
+              Medical Content Researcher
+            </h1>
+          </div>
+          <div class='py-24'>
+            <h2 class='text-2xl font-extralight'>
+              I compile high-quality medical research and summarize it in
+              everyday language. I work for writers and individuals with a
+              vested interest in what the studies mean for life on the ground.
+            </h2>
+          </div>
+        </div>
+        <div class='w-2/6 bg-with-image  flex items-center justify-center'></div>
+      </div>
+
+      <div class='flex flex-col '>
+        <div class='flex flex-row border-b border-slate-800 px-12'>
+          <p class='text-2xl font-extralight my-5'>Background</p>
+        </div>
+        <div class='flex flex-row border-b border-slate-800'>
+          <div class='px-12 w-1/2 '>
+            <p class='top-0 sticky font-extralight text-2xl my-5'>Education</p>
+          </div>
+
+          <div class='flex flex-col w-1/2 border-l border-slate-800'>
+            <div class='border-b border-slate-800 p-5'>
+              <h1 class='text-2xl font-extralight '>BA Biology</h1>
+            </div>
+            <div class='p-5'>
+              <h1 class='text-2xl font-extralight'>
+                MA Counseling Psychology
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div class='flex flex-row border-b border-slate-800'>
+          <div class='px-12 w-1/2'>
+            <p class='top-0 sticky text-2xl font-extralight my-5'>
+              Professional
+            </p>
+          </div>
+
+          <div class='flex flex-col h-90vh w-1/2 border-l border-slate-800'>
+            {titles.map((item) => (
+              <Title item={item} key={item.title} />
+            ))}
+            <motion.div className='progress' style={{ scaleX }} />
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div class='flex flex-col h-screen px-12 justify-center'>
+        <h2 class='text-2xl'>
+          Find out how I can help you put the best evidence into context.
+        </h2>
+        <h2 class='text-2xl'>contact me</h2>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
