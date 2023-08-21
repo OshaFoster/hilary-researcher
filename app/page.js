1cd41d54ceed0a3680ddd87a6f6a7a52ceb5e7f8
Osha Foster <31053423+OshaFoster@users.noreply.github.com>
2023-08-21 13:26:18 -0600

    requested updates

--- app/page.js
+++ app/page.js
@@ -4,14 +4,13 @@ import { motion, useScroll, useSpring } from 'framer-motion';
 import CustomCursor from './components/customCursor';
 const titles = [
   {
-    title: 'Medical content researcher and reviewer for The Ruscio Institute',
-    years: '3yrs',
+    title: 'Medical content researcher & reviewer for The Ruscio Institute',
+    years: '3 y',
   },
-  { title: 'Biologist', years: '10yrs' },
-  { title: 'Editor and writer', years: '11yrs' },
-  { title: 'Journalist', years: '2yrs' },
-  { title: 'Student of medicine', years: '2yrs' },
-  { title: 'Marriage & Family Therapist trainee', years: '1yr' },
+  { title: 'Biologist', years: '10 y' },
+  { title: 'Editor & writer', years: '11 y' },
+  { title: 'Journalist', years: '2 y' },
+  { title: 'Marriage & Family Therapist trainee', years: '1 y' },
 ];
 
 // <div className='flex flex-wrap flex-row max-w-sm  md:max-w-2xl p-12 pr-7'>
@@ -21,7 +20,7 @@ function Title({ item }) {
 
   return (
     <section className='flex flex-row border-b border-slate-800'>
-      <div className='flex flex-wrap flex-row max-w-md  md:max-w-2xl pl-6 sm:pl-12 p-7'>
+      <div className='flex flex-wrap flex-row max-w-md  md:max-w-xl pl-6 sm:pl-12 p-7'>
         <h1 className='text-xl sm:text-2xl font-extralight'>{item.title}</h1>
       </div>
       <div className='flex flex-row pt-7' ref={ref}>
@@ -105,10 +104,10 @@ export default function Home() {
               transition={{ duration: 0.3, delay: 0.4 }}
               className=' md:py-6 top-0 sticky z-40 bg-zinc-50 my-1'
             >
-              <h1 className='font-victor text-5xl sm:text-6xl text-sky-800'>
+              <h1 className='font-victor text-6xl sm:text-6xl text-lime-700'>
                 Hilary Watts
               </h1>
-              <h1 className='text-xl sm:text-2xl  font-victor text-sky-800'>
+              <h1 className='text-xl sm:text-2xl  font-victor text-lime-700'>
                 Medical Content Researcher
               </h1>
             </motion.div>
@@ -124,8 +123,6 @@ export default function Home() {
         </div>
 
         <div className='flex flex-col'>
-
-
           <div className='flex flex-col  md:flex-row border-b border-slate-800'>
             <div className='px-6 sm:px-12 w-full md:w-1/2 bg-pink-700 sm:bg-zinc-50'>
               <p className='top-5 sticky text-xl sm:text-2xl my-5  text-zinc-50 sm:text-slate-800'>
@@ -135,12 +132,29 @@ export default function Home() {
 
             <div className='flex flex-col w-full md:w-1/2 border-l border-slate-800'>
               <div className='border-b border-slate-800 p-7 pl-6 sm:pl-12'>
-                <h1 className='text-xl sm:text-2xl font-extralight '>BA Biology</h1>
+                <h1 className='text-xl sm:text-2xl font-extralight '>
+                  BA Biology
+                </h1>
               </div>
-              <div className='p-7 pl-6 sm:pl-12'>
+              <div className='p-7 pl-6 sm:pl-12 border-b border-slate-800'>
                 <h1 className='text-xl sm:text-2xl font-extralight'>
                   MA Counseling Psychology
                 </h1>
+              </div>
+              <div className='flex flex-row p-7  pl-6 sm:pl-12'>
+                <h1 className='text-xl sm:text-2xl font-extralight pr-6'>
+                  Student of Medicine
+                </h1>
+             
+                  <motion.h2
+                    initial={{ opacity: 0, y: -80 }}
+                    whileInView={{ opacity: 1, y: 1 }}
+                    transition={{ duration: 0.3, delay: 0.8 }}
+                    className='text-xl sm:text-2xl font-victor font-medium text-pink-700 mr-10'
+                  >
+                    2 y
+                  </motion.h2>
+               
               </div>
             </div>
           </div>
@@ -162,7 +176,7 @@ export default function Home() {
         </div>
 
         <div className='flex h-screen flex-col justify-center items-center '>
-          <h2 className='text-3xl font-victor mx-12 text-center text-slate-500'>
+          <h2 className='text-3xl font-victor mx-12 text-center text-lime-700'>
             Find out how I can help you put the best evidence into context.
           </h2>
           <EmailLink />
