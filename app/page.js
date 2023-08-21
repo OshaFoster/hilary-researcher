491bcb9218c450544f4def3da21b6232ff25440a
Osha Foster <31053423+OshaFoster@users.noreply.github.com>
2023-08-21 12:28:56 -0600

    pink-700, cyan-800

--- app/page.js
+++ app/page.js
@@ -15,21 +15,21 @@ const titles = [
 ];
 
 // <div className='flex flex-wrap flex-row max-w-sm  md:max-w-2xl p-12 pr-7'>
-//   <h1 className='text-xl font-extralight whitespace-normal'>{item.title}</h1>
+//   <h1 className='text-xl sm:text-2xl font-extralight whitespace-normal'>{item.title}</h1>
 function Title({ item }) {
   const ref = useRef(null); // Adjust the start and end values
 
   return (
     <section className='flex flex-row border-b border-slate-800'>
       <div className='flex flex-wrap flex-row max-w-md  md:max-w-2xl pl-6 sm:pl-12 p-7'>
-        <h1 className='text-xl font-extralight'>{item.title}</h1>
+        <h1 className='text-xl sm:text-2xl font-extralight'>{item.title}</h1>
       </div>
       <div className='flex flex-row pt-7' ref={ref}>
         <motion.h2
           initial={{ opacity: 0, y: -80 }}
           whileInView={{ opacity: 1, y: 1 }}
           transition={{ duration: 0.3, delay: 0.8 }}
-          className='text-xl font-victor font-medium text-pink-700 mr-10'
+          className='text-xl sm:text-2xl font-victor font-medium text-pink-700 mr-10'
         >
           {item.years}
         </motion.h2>
@@ -59,7 +59,7 @@ export default function Home() {
         className='contact bg-zinc-50 h-16 flex justify-center items-center border w-80 sm:w-96 mt-16 relative p-6 border-solid border-r-4 border-b-4 border-slate-700 hover:border-slate-800 sm:hover:border-emerald-800'
         href={mailtoLink}
       >
-        <p className='text-xl font-medium font-victor text-pink-700'>
+        <p className='text-xl sm:text-2xl font-medium font-victor text-pink-700'>
           Contact Me
         </p>
       </motion.a>
@@ -79,7 +79,7 @@ export default function Home() {
         <div className='border-r border-b border-slate-800 flex flex-row h-90vh'>
           <div className='px-6 sm:px-12 flex flex-col w-5/6 md:w-4/6 border-r border-slate-800'>
             <div className='py-6 sm:py-24'>
-              <h2 className='text-xl font-light'>
+              <h2 className='text-xl sm:text-2xl font-light'>
                 Medical research is{' '}
                 <span className='font-normal text-pink-700'>
                   hard to interpret
@@ -105,15 +105,15 @@ export default function Home() {
               transition={{ duration: 0.3, delay: 0.4 }}
               className=' md:py-6 top-0 sticky z-40 bg-zinc-50 my-1'
             >
-              <h1 className='font-victor text-5xl  text-lime-700'>
+              <h1 className='font-victor text-5xl sm:text-6xl text-cyan-800'>
                 Hilary Watts
               </h1>
-              <h1 className='text-xl  font-victor text-lime-700'>
+              <h1 className='text-xl sm:text-2xl  font-victor text-cyan-800'>
                 Medical Content Researcher
               </h1>
             </motion.div>
             <div className='py-6 sm:py-24'>
-              <h2 className='text-xl font-light'>
+              <h2 className='text-xl sm:text-2xl font-light'>
                 I compile high-quality medical research and summarize it in
                 everyday language. I work for writers and individuals with a
                 vested interest in what the studies mean for life on the ground.
@@ -127,18 +127,18 @@ export default function Home() {
 
 
           <div className='flex flex-col  md:flex-row border-b border-slate-800'>
-            <div className='px-6 sm:px-12 w-full md:w-1/2 bg-pink-800 sm:bg-zinc-50'>
-              <p className='top-5 sticky text-xl my-5  text-zinc-50 sm:text-slate-800'>
+            <div className='px-6 sm:px-12 w-full md:w-1/2 bg-pink-700 sm:bg-zinc-50'>
+              <p className='top-5 sticky text-xl sm:text-2xl my-5  text-zinc-50 sm:text-slate-800'>
                 Education
               </p>
             </div>
 
             <div className='flex flex-col w-full md:w-1/2 border-l border-slate-800'>
               <div className='border-b border-slate-800 p-7 pl-6 sm:pl-12'>
-                <h1 className='text-xl font-extralight '>BA Biology</h1>
+                <h1 className='text-xl sm:text-2xl font-extralight '>BA Biology</h1>
               </div>
               <div className='p-7 pl-6 sm:pl-12'>
-                <h1 className='text-xl font-extralight'>
+                <h1 className='text-xl sm:text-2xl font-extralight'>
                   MA Counseling Psychology
                 </h1>
               </div>
@@ -146,8 +146,8 @@ export default function Home() {
           </div>
 
           <div className='flex flex-col  md:flex-row '>
-            <div className='pl-6 sm:pl-12 w-full md:w-1/2 border-b border-slate-800 bg-pink-800 sm:bg-zinc-50'>
-              <p className='top-5 sticky text-xl my-5 text-turquise text-zinc-50 sm:text-slate-800'>
+            <div className='pl-6 sm:pl-12 w-full md:w-1/2 border-b border-slate-800 bg-pink-700 sm:bg-zinc-50'>
+              <p className='top-5 sticky text-xl sm:text-2xl my-5 text-turquise text-zinc-50 sm:text-slate-800'>
                 Professional
               </p>
             </div>
@@ -162,7 +162,7 @@ export default function Home() {
         </div>
 
         <div className='flex h-screen flex-col justify-center items-center '>
-          <h2 className='text-3xl font-victor mx-12 text-center text-lime-700'>
+          <h2 className='text-3xl font-victor mx-12 text-center text-cyan-800'>
             Find out how I can help you put the best evidence into context.
           </h2>
           <EmailLink />
